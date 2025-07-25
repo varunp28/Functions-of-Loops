648c2e2cd9733d4a35d430a04d1d73877c4a823a
varunp28 <varunparandkar@gmail.com>
2025-07-25 09:18:39 +0530

    Create Print Even Number.cpp

---
+++ Print Even Number.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    for(int i=0; i<11;i=i+2){
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
+
+OutPut: 
+0
+2
+4
+6
+8
+10
