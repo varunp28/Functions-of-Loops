fbfe573334037036d716af47c278eef0f37e54ae
varunp28 <varunparandkar@gmail.com>
2025-07-25 09:34:11 +0530

    Create print name.cpp

---
+++ print name.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string a="messi";
+    for(int i=0;i<=4;i++){
+        cout<< a <<endl;
+    }
+}
+
+//Output
+//messi
+//messi
+//messi
+//messi
+//messi
